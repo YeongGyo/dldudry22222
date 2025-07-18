4c057f4c3b3cd1cd23c5d3acc39ae77fa90d912c
yeonggyo <dldudry2222@gmail.com>
2025-07-18 15:16:42 +0900

    asd123

---
+++ dldudry22222/src/main/java/com/example/demo/Dldudry22222Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dldudry22222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dldudry22222Application.class, args);
+	}
+
+}
